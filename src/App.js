fda09baea492153f519033a2d7b535e4461f637c
TOMOKI O <tomokiokuro@gmail.com>
2020-06-21 01:47:04 +0900

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import Title from './components/Title/Title'
+
+class App extends Component {
+  state = {version: '1.0'}
+
+  onClickHandler = () => {
+    let nextVersion = parseInt(this.state.version, 10) + 1;
+    this.setState({version: nextVersion.toFixed(1)})
+}
+
+  render(){
+    let upgradeButton = (
+      <p
+        onClick   = {this.onClickHandler}
+        id        = "upgradeButton"
+        className = "upgrade-button"
+        >Upgrade</p>
+    )
+
+    if(this.state.version === '5.0'){
+      upgradeButton = (
+        <p
+          className = "upgraded-button"
+        >Already up-to-date</p>
+      )
+    }
+
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Title
+          style   = {{color: 'orange'}}
+          onClick = {this.onClickHandler}
+        >Hello World <span id = "versionCounter" style = {{borderBottom: '1px solid orange'}}>
+          {this.state.version}</span>
+        </Title>
+
+        {upgradeButton}
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,8 +55,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
